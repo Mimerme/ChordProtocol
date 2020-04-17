397ead7772f8f28fcc3fb73df8bd15f529640019
Andros Yang <androssdragonq.9@gmail.com>
2020-04-16 22:53:35 -0400

    Added poisx argument parsing

--- ChordProtocol/src/MainCreate.java
+++ ChordProtocol/src/MainCreate.java
@@ -5,11 +5,12 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
 
 public class MainCreate {
 
 	public static void main(String[] args) throws Exception {
-		CommandLineParser parser = new DefaultParser();
+		CommandLineParser parser = new PosixParser();
 		Options options = new Options();
 		options.addRequiredOption( "a", "address", true, "The IP address that the Chord client will bind to, as well as advertise to other nodes" );
 		options.addRequiredOption( "p", "port",true, "The port that the Chord client will bind to and listen on." );
